9abd4043318251d7b00639d9597abe827ec5263a
Zeeshan Younas <136065779+Zeeshan5932@users.noreply.github.com>
2026-02-14 21:34:11 +0500

    new

--- backend/main.py
+++ backend/main.py
@@ -4,6 +4,10 @@ from fastapi import FastAPI,File,UploadFile
 from PIL import Image
 from fastapi.middleware.cors import CORSMiddleware
 
+#load model
+model = tf.keras.models.load_model('./model/skin_cancer_model_final.h5')
 
+#class labels
+class_names = ["Benign" , "Malignant"]
 
 app = FastAPI()
