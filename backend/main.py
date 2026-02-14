97a74a77ca218a73fe675939382c5afa616f3320
Zeeshan Younas <136065779+Zeeshan5932@users.noreply.github.com>
2026-02-14 21:37:22 +0500

    new

--- backend/main.py
+++ backend/main.py
@@ -11,3 +11,26 @@ model = tf.keras.models.load_model('./model/skin_cancer_model_final.h5')
 class_names = ["Benign" , "Malignant"]
 
 app = FastAPI()
+
+# allow frontend access
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_methods=["*"],
+    allow_headers=["*"],
+        allow_credentials=True
+)
+
+
+#image preprocessing function
+def preprocess_imaage(image):
+    image = image.resize((224,224))
+    image = np.array(image)
+    image = image / 255.0
+    image = np.expand_dims(image, axis=0)
+    return image
+
+
+
+
